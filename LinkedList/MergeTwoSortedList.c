175cdbf8768abd6178570ee418bb5e4d4bd831eb
walid885 <bettaieb88walid@.com>
2023-10-20 23:12:09 +0100

    playing arround and setting the keybingind for compiling and executing

---
+++ LinkedList/MergeTwoSortedList.c
@@ -0,0 +1,23 @@
+/** this is the program that solves the merging of two sorted linked List
+the problem can be found on this link : https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+/*
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+printf("is this working ? ");
+}
+*/
+int main () {
+int a,b; 
+a= 1 ; 
+b = 2 ; 
+printf("%d",&a);
+
+}
